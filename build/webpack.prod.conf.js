c9040481a0d4cb7610edf26133cfcabd92342454
Dani Biró <danibiro93@gmail.com>
2018-08-21 13:03:20 +0300

    Fix production build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -54,7 +54,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap ? { safe: true, map: { inline: false } } : { safe: true }
+      cssProcessorOptions: config.build.productionSourceMap ? { map: { inline: false } } : { }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
