f827d67c319009e086fd36bd76d83962937716fa
Dani Biró <danibiro93@gmail.com>
2018-10-16 12:56:11 +0300

    Use demo horizon server.

--- src/config/demo.js
+++ src/config/demo.js
@@ -4,6 +4,6 @@ const nodeScript = __NODE_SCRIPT__;
 export default {
   API_BASE: nodeScript === 'dev-local' ? 'http://localhost:9002' : 'https://demoapi.lumenshine.com',
 
-  HORIZON_URL: 'https://horizon-testnet.stellar.org',
+  HORIZON_URL: 'https://demohorizon.lumenshine.com',
   IS_TEST_NETWORK: true,
 };
