94b7c6031148087ab5550331328699da178edbe1
Dani Biró <danibiro93@gmail.com>
2018-10-19 12:30:44 +0300

    Refactor Api Urls, add data urls.

--- src/services/apiUrls.js
+++ src/services/apiUrls.js
@@ -1,11 +1,15 @@
 // import queryString from '../../node_modules/query-string';
 
-function app (url) {
+function appPrefix (url) {
   return '/portal/admin/dash/' + url;
 }
 
-function anon (url) {
+function anonPrefix (url) {
   return '/portal/admin/' + url;
+}
+
+function userPrefix (url) {
+  return '/portal/user/' + url;
 }
 
 class UrlReplacer {
@@ -33,7 +37,7 @@ class UrlReplacer {
     }
 
     if (withApp) {
-      return app(retUrl);
+      return appPrefix(retUrl);
     }
     return retUrl;
   }
@@ -43,71 +47,78 @@ class UrlReplacer {
 // all plain urls (except auth) must be prefixed with app, because thats the protected route in the server
 
 const Urls = {
+  Data: {
+    SalutationList: userPrefix('salutation_list'),
+    CountryList: userPrefix('country_list'),
+    OccupationList: userPrefix('occupation_list'),
+    LanguageList: userPrefix('language_list'),
+  },
+
   Auth: {
-    Login: anon('login'),
-    Refresh: app('refresh'),
-    UserData: app('user/user_data')
+    Login: anonPrefix('login'),
+    Refresh: appPrefix('refresh'),
+    UserData: appPrefix('user/user_data')
   },
 
   Users: {
-    AllUsers: app('user/list'),
-    GetUser: app('user/details'),
-    EditUser: app('user/edit'),
-    SetGroups: app('user/setgroups'),
-    RegisterUser: app('user/register'),
-    ChangePassword: app('user/change-password'),
-    ResetPassword: anon('user/reset-password')
+    AllUsers: appPrefix('user/list'),
+    GetUser: appPrefix('user/details'),
+    EditUser: appPrefix('user/edit'),
+    SetGroups: appPrefix('user/setgroups'),
+    RegisterUser: appPrefix('user/register'),
+    ChangePassword: appPrefix('user/change-password'),
+    ResetPassword: anonPrefix('user/reset-password')
   },
 
   Accounts: {
-    AllAccounts: app('stellar_account/accounts_list'),
-    GetAccount: app('stellar_account/details'),
-    AddAccount: app('stellar_account/add'),
-    RemoveAccount: app('stellar_account/remove'),
-    EditAccount: app('stellar_account/edit'),
+    AllAccounts: appPrefix('stellar_account/accounts_list'),
+    GetAccount: appPrefix('stellar_account/details'),
+    AddAccount: appPrefix('stellar_account/add'),
+    RemoveAccount: appPrefix('stellar_account/remove'),
+    EditAccount: appPrefix('stellar_account/edit'),
 
-    AddAssetCode: app('stellar_account/add_asset_code'),
-    RemoveAssetCode: app('stellar_account/remove_asset_code'),
+    AddAssetCode: appPrefix('stellar_account/add_asset_code'),
+    RemoveAssetCode: appPrefix('stellar_account/remove_asset_code'),
 
-    AddAllowTrustSigner: app('stellar_account/add_allowtrust_signer'),
-    RemoveAllowTrustSigner: app('stellar_account/remove_allowtrust_signer'),
-    EditAllowTrustSigner: app('stellar_account/edit_allowtrust_signer'),
-    AddOtherSigner: app('stellar_account/add_other_signer'),
-    RemoveOtherSigner: app('stellar_account/remove_other_signer'),
-    EditOtherSigner: app('stellar_account/edit_other_signer'),
+    AddAllowTrustSigner: appPrefix('stellar_account/add_allowtrust_signer'),
+    RemoveAllowTrustSigner: appPrefix('stellar_account/remove_allowtrust_signer'),
+    EditAllowTrustSigner: appPrefix('stellar_account/edit_allowtrust_signer'),
+    AddOtherSigner: appPrefix('stellar_account/add_other_signer'),
+    RemoveOtherSigner: appPrefix('stellar_account/remove_other_signer'),
+    EditOtherSigner: appPrefix('stellar_account/edit_other_signer'),
 
-    SearchTrustingAccounts: app('stellar_account/search_trusting_accounts'),
-    SearchWorkerAccountTrustlines: app('stellar_account/worker_account_trustlines'),
-    AddUnauthorizedTrustline: app('stellar_account/add_unauthorized_trustline'),
-    RemoveUnauthorizedTrustline: app('stellar_account/remove_unauthorized_trustline'),
+    SearchTrustingAccounts: appPrefix('stellar_account/search_trusting_accounts'),
+    SearchWorkerAccountTrustlines: appPrefix('stellar_account/worker_account_trustlines'),
+    AddUnauthorizedTrustline: appPrefix('stellar_account/add_unauthorized_trustline'),
+    RemoveUnauthorizedTrustline: appPrefix('stellar_account/remove_unauthorized_trustline'),
   },
 
   Customers: {
-    AllCustomers: app('customer/list'),
-    GetCustomerDetails: app('customer/details'),
-    GetCustomerOrders: app('customer/orders'),
-    GetCustomerWallets: app('customer/wallets'),
-    EditCustomer: app('customer/update_personal_data'),
-    UpdateKYCStatus: app('customer/update_kyc_status'),
-    Reset2FA: app('customer/reset2fa'),
+    AllCustomers: appPrefix('customer/list'),
+    GetCustomerDetails: appPrefix('customer/details'),
+    GetCustomerOrders: appPrefix('customer/orders'),
+    GetCustomerWallets: appPrefix('customer/wallets'),
+    EditCustomer: appPrefix('customer/update_personal_data'),
+    UpdateKYCStatus: appPrefix('customer/update_kyc_status'),
+    Reset2FA: appPrefix('customer/reset2fa'),
   },
 
   KnownCurrencies: {
-    AllCurrencies: app('known_currencies/all'),
-    GetCurrency: app('known_currencies/get'),
-    AddCurrency: app('known_currencies/add'),
-    EditCurrency: app('known_currencies/edit'),
-    DeleteCurrency: app('known_currencies/delete'),
-    ChangeCurrencyOrder: app('known_currencies/changeOrder'),
+    AllCurrencies: appPrefix('known_currencies/all'),
+    GetCurrency: appPrefix('known_currencies/get'),
+    AddCurrency: appPrefix('known_currencies/add'),
+    EditCurrency: appPrefix('known_currencies/edit'),
+    DeleteCurrency: appPrefix('known_currencies/delete'),
+    ChangeCurrencyOrder: appPrefix('known_currencies/changeOrder'),
   },
 
   KnownInflationDestinations: {
-    AllInflationDestinations: app('known_inflation_destinations/all'),
-    GetInflationDestination: app('known_inflation_destinations/get'),
-    AddInflationDestination: app('known_inflation_destinations/add'),
-    EditInflationDestination: app('known_inflation_destinations/edit'),
-    DeleteInflationDestination: app('known_inflation_destinations/delete'),
-    ChangeInflationDestinationOrder: app('known_inflation_destinations/changeOrder'),
+    AllInflationDestinations: appPrefix('known_inflation_destinations/all'),
+    GetInflationDestination: appPrefix('known_inflation_destinations/get'),
+    AddInflationDestination: appPrefix('known_inflation_destinations/add'),
+    EditInflationDestination: appPrefix('known_inflation_destinations/edit'),
+    DeleteInflationDestination: appPrefix('known_inflation_destinations/delete'),
+    ChangeInflationDestinationOrder: appPrefix('known_inflation_destinations/changeOrder'),
   },
 
   Tasks: {
